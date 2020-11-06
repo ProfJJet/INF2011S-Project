bfc10656a63c62b59997ceabeb43848e17c39907
Jeremy McConnell <jj@aaa.net.au>
2020-11-06 11:07:29 +0200

    Minor entity adjustments and unfilled Database added to the server

--- Poppel_Ordering_System/Entities/Order.cs
+++ Poppel_Ordering_System/Entities/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,12 @@ namespace Poppel_Ordering_System.Entities
     {
         #region Order Fields
         private int orderNum;
+        private Customer customer;
         private DateTime datePlaced;
         private DateTime dateShipped;
+        private String deliveryAddress;
         private OrderStatus status;
-        private OrderItem[] items;
+        private Collection<OrderItem> items;
         #endregion
 
 
@@ -25,7 +28,11 @@ namespace Poppel_Ordering_System.Entities
             get { return orderNum; }
             set { orderNum = value; }
         }
-
+        public Customer Customer 
+        {
+            get { return customer ; }
+            set { customer  = value; }
+        }
         public DateTime DatePlaced
         {
             get { return datePlaced; }
@@ -38,16 +45,44 @@ namespace Poppel_Ordering_System.Entities
             set { dateShipped = value; }
         }
 
+        public String DeliveryAddress
+        {
+            get { return deliveryAddress; }
+            set { deliveryAddress = value; }
+        }
         public OrderStatus Status
         {
             get { return status; }
             set { status = value; }
         }
 
-        public OrderItem[]  Items
+        public Collection<OrderItem>  Items
         {
             get { return items ; }
             set { items  = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Order( int orderNum, Customer customer, DateTime datePlaced, DateTime dateShipped, String deliveryAddress, OrderStatus status, Collection<OrderItem> items )
+        {
+            this.OrderNum = orderNum;
+            this.Customer = customer;
+            this.DatePlaced = datePlaced;
+            this.DateShipped = dateShipped;
+            this.DeliveryAddress = deliveryAddress;
+            this.Status = status;
+            this.Items = items;
+        }
+        public Order()
+        {
+            this.OrderNum = 0;
+            this.Customer = null;
+            this.DatePlaced = DateTime.MinValue;
+            this.DateShipped = DateTime.MinValue;
+            this.DeliveryAddress = "";
+            this.Status = OrderStatus.New_Order;
+            this.Items = null;
         }
         #endregion
     }
